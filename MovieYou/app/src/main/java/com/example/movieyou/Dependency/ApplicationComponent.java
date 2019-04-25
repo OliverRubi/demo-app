f0f137ae9cf262710cc2fd59466690a05248592c
John Oliver B. Rubi <olibrubi29@gmail.com>
2019-04-26 01:23:20 +0800

    eee

--- MovieYou/app/src/main/java/com/example/movieyou/Dependency/ApplicationComponent.java
+++ MovieYou/app/src/main/java/com/example/movieyou/Dependency/ApplicationComponent.java
@@ -1,6 +1,7 @@
 package com.example.movieyou.Dependency;
 
 import com.example.movieyou.Fragments.Home.HomeFragmentPresenter;
+import com.example.movieyou.ui.AboutMovie;
 import com.example.movieyou.ui.SeeAllMoviesActivity;
 
 import javax.inject.Singleton;
@@ -13,4 +14,5 @@ public interface ApplicationComponent {
 
     void inject(HomeFragmentPresenter target);
     void inject(SeeAllMoviesActivity target);
+    void inject(AboutMovie target);
 }
--- MovieYou/app/src/main/java/com/example/movieyou/ui/AboutMovie.java
+++ MovieYou/app/src/main/java/com/example/movieyou/ui/AboutMovie.java
@@ -1,16 +1,68 @@
 package com.example.movieyou.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.appcompat.widget.Toolbar;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.movieyou.Dependency.App;
+import com.example.movieyou.R;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class AboutMovie extends AppCompatActivity {
 
+    @BindView(R.id.aboutCotainer)
+    ViewPager container;
+    @BindView(R.id.nameTextView)
+    TextView movieTitle;
+    @BindView(R.id.releaseDateTextView)
+    TextView releaseDate;
+    @BindView(R.id.runTimeTextView)
+    TextView runtime;
+    @BindView(R.id.movieThumbnailImageView)
+    ImageView poster;
+    @BindView(R.id.genreTextView)
+    TextView genres;
+    @BindView(R.id.ratingTextView)
+    TextView rating;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.collapsing_toolbar)
+    CollapsingToolbarLayout collapsingToolbarLayout;
+
+    int movie_id;
+    String movieName;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       // setContentView(R.layout.see_all_content_movie);
+       setContentView(R.layout.about_movie);
+
+        ((App) getApplication()).getComponent().inject(this);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
+        Intent intent = getIntent();
+        movie_id = intent.getIntExtra("movie_id", 0);
+        final String posterPath = intent.getStringExtra("poster_path");
+        movieName = intent.getStringExtra("movie_name");
+
+        collapsingToolbarLayout.setTitle("About Movie");
+        collapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(android.R.color.transparent, getTheme()));
+
+
+
     }
 }
